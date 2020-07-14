bb7f7b7942d4bbea3b1f9a5971b4a1728d5c998a
BenWhite101 <ben.white101@hotmail.co.uk>
2020-07-14 20:18:31 +0100

    Huge changes and build

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-const fff = document.querySelector('filters-wrap');
+const filtersWrap = document.querySelector('filters-wrap');
 let requestURL = 'https://musing-dubinsky-734d44.netlify.app/data.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
@@ -13,14 +13,20 @@ request.onload = function() {
     function populate(jsonObj){
         jsonObj.forEach(item => {
             /* START FOREACH */
-
-            console.log(item.company);
             
             let id = item.id;
             let company = item.company;
             let logoUrl = item.logo;
             let isNew = item.new;
+            if (isNew) {
+                let isNewText = `<span class="badge new">New</span>`
+            }
+
             let isFeatured = item.featured;
+            if (isFeatured) {
+                let isFeaturedText = `<span class="badge featured">Featured</span>`
+            }
+
             let position = item.position;
             let role = item.role;
             let level = item.level;
@@ -28,34 +34,48 @@ request.onload = function() {
             let contract = item.contract;
             let location = item.location;
 
-            
-
-            let languages = item.languages;
-            let allLang = "";
-
-            item.languages.forEach(item => {
-                
-                allLang += `<button>${item}</button>`;
-            });
-            console.log(allLang);
-
             let tools = item.tools;
-
-            //"id": 1,
-            //"company": "Photosnap",
-            //"logo": "./images/photosnap.svg",
-            //"new": true,
-            //"featured": true,
-            //"position": "Senior Frontend Developer",
-            //"role": "Frontend",
-            //"level": "Senior",
-            //"postedAt": "1d ago",
-            //"contract": "Full Time",
-            //"location": "USA Only",
-            //"languages": ["HTML", "CSS", "JavaScript"],
-            //"tools": []
+            let languages = item.languages.concat(tools);
+                let allLang = "";
+                item.languages.forEach(item => {
+                    allLang += `<button>${item}</button>`;
+                });
 
 
+
+            let main = "";
+
+             main += 
+             `
+                <div class="block">
+                <div class="wrap">
+
+                    <div class="left">
+                    <img src="${logoUrl}" alt="${logoUrl} Logo">
+                    </div>
+                    <div class="mid">
+                    <div class="top">
+                        <span class="company">${company}</span>
+                        <span class="badges">
+                            ${isNewText}
+                            ${isFeaturedText}
+                        </span>
+                    </div>
+                    <h6>${position}</h6>
+                    <div class="bottom">
+                        <span>${postedAt} - ${contract} - ${location}</span>
+                    </div>
+                    </div>
+                    <div class="right">
+                        <div class="buttons">
+                            ${allLang}
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+
+             `   
 
 
 

049116ff4a5146e4a8aa36fc2d8fe4c0aa2c6129
BenWhite101 <ben.white101@hotmail.co.uk>
2020-07-14 19:04:07 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -11,7 +11,7 @@ request.onload = function() {
     populate(jobsList);
 
     function populate(jsonObj){
-        jsonObj.array.forEach(item => {
+        jsonObj.forEach(item => {
             console.log(item.company);
         });
       }

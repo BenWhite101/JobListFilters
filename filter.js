9ba10fdf8cd28f53644d8386e5b0fef2da68d9b6
BenWhite101 <ben.white101@hotmail.co.uk>
2020-07-14 19:28:48 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -12,7 +12,51 @@ request.onload = function() {
 
     function populate(jsonObj){
         jsonObj.forEach(item => {
+            /* START FOREACH */
+
             console.log(item.company);
+            
+            let id = item.id;
+            let company = item.company;
+            let logoUrl = item.logo;
+            let isNew = item.new;
+            let isFeatured = item.featured;
+            let position = item.position;
+            let role = item.role;
+            let level = item.level;
+            let postedAt = item.postedAt;
+            let contract = item.contract;
+            let location = item.location;
+
+            item.location.forEach(item => {
+                console.log(item);
+            });
+
+            let languages = item.languages;
+            let tools = item.tools;
+
+            //"id": 1,
+            //"company": "Photosnap",
+            //"logo": "./images/photosnap.svg",
+            //"new": true,
+            //"featured": true,
+            //"position": "Senior Frontend Developer",
+            //"role": "Frontend",
+            //"level": "Senior",
+            //"postedAt": "1d ago",
+            //"contract": "Full Time",
+            //"location": "USA Only",
+            //"languages": ["HTML", "CSS", "JavaScript"],
+            //"tools": []
+
+
+
+
+
+
+
+
+            /* END FOREACH */
         });
       }
     

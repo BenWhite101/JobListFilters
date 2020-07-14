f8665c94eff3c1dc29bd0c741914de2199cdd173
BenWhite101 <ben.white101@hotmail.co.uk>
2020-07-14 18:59:54 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -2,18 +2,20 @@ const fff = document.querySelector('filters-wrap');
 let requestURL = 'https://musing-dubinsky-734d44.netlify.app/data.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType = 'json';
+request.responseType = 'text';
 request.send();
 request.onload = function() {
-    const jobsList = request.response;
-   
+    const jobsListJ = request.response;
+    const jobsList = JSON.parse(jobsListJ);
 
     populate(jobsList);
+
+    function populate(jsonObj){
+        jsonObj.array.forEach(item => {
+            console.log(item.company);
+        });
+      }
     
   }
 
-  function populate(jsonObj){
-    jsonObj.array.forEach(item => {
-        console.log(item.company);
-    });
-  }
+  
